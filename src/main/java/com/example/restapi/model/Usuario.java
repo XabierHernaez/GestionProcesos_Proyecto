2693eb6c50c8388f0b282678dc06ab1883859016
Adrian San Jose <115790325+AdrianSanJoseGonzalez@users.noreply.github.com>
2025-03-28 20:13:04 +0100

    Creacion Clase Usuario

---
+++ src/main/java/com/example/restapi/model/Usuario.java
@@ -0,0 +1,108 @@
+package com.example.restapi.model;
+
+import java.sql.Date;
+
+public class Usuario {
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String password;
+    private String telefono;
+    private Long dni;
+    private Date fechaNacimiento;
+    private TipoPago tipoPago;
+
+    // Constructor vacío
+    public Usuario() {}
+
+    // Constructor con parámetros
+    public Usuario(String nombre, String apellidos, String email, String password, String telefono, Long dni, Date fechaNacimiento, TipoPago tipoPago) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.password = password;
+        this.telefono = telefono;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoPago = tipoPago;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Long getDni() {
+        return dni;
+    }
+
+    public void setDni(Long dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public TipoPago getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(TipoPago tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", email='" + email + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", dni=" + dni +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", tipoPago=" + tipoPago +
+                '}';
+    }
+}
