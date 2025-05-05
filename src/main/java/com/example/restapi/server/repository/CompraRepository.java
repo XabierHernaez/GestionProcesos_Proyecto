7d0291424dd05d41a6fd7718c23d2472b041f961
Xabier Hernaez <xabier.h.s@opendeusto.es>
2025-05-05 10:04:21 +0200

    Ventana mis compras hecho

--- src/main/java/com/example/restapi/server/repository/CompraRepository.java
+++ src/main/java/com/example/restapi/server/repository/CompraRepository.java
@@ -1,9 +1,9 @@
 package com.example.restapi.server.repository;
 
 import com.example.restapi.server.jpa.CompraJPA;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CompraRepository extends JpaRepository<CompraJPA, Long>{
-    
+    List<CompraJPA> findByUsuarioEmail(String email);
 }
