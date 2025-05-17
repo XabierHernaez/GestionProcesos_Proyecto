663985262c6cae77884b1691284797225039d1ca
Xabier Hernaez <xabier.h.s@opendeusto.es>
2025-05-17 12:01:50 +0200

    Doxigen añadido

--- src/main/java/com/example/restapi/model/TipoAsiento.java
+++ src/main/java/com/example/restapi/model/TipoAsiento.java
@@ -1,5 +1,17 @@
 package com.example.restapi.model;
 
+/**
+ * @enum TipoAsiento
+ * @brief Enum que representa los tipos de asientos disponibles en un concierto.
+ *
+ * Tipos de asientos que determinan la ubicación y posiblemente los servicios asociados:
+ * - VIP: Asientos en la zona VIP, con mejores comodidades.
+ * - GENERAL: Asientos estándar en la zona general.
+ */
 public enum TipoAsiento {
-    VIP, GENERAL
+    /** Asiento en zona VIP con mejores comodidades */
+    VIP,
+
+    /** Asiento estándar en zona general */
+    GENERAL
 }
